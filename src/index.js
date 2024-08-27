e278f92ea9b30584086aaa1d43ba1737bd19eee6
Christian Schmitz <cschmitz398@gmail.com>
2024-08-27 16:18:14 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,61 @@
+export * as Crypto from "@helios-lang/crypto"
+export {
+    encodeIntBE as bigIntToBytes,
+    bytesToHex,
+    hexToBytes,
+    encodeUtf8 as textToBytes,
+    decodeUtf8 as bytesToText
+} from "@helios-lang/codec-utils"
+export { Program } from "@helios-lang/compiler"
+import { DEFAULT_NETWORK_PARAMS } from "@helios-lang/ledger"
+export {
+    Address,
+    AssetClass,
+    Assets,
+    MintingPolicyHash,
+    PubKey,
+    PubKeyHash,
+    ScriptHash,
+    Signature,
+    StakingValidatorHash,
+    StakingAddress as StakeAddress,
+    Tx,
+    TxId,
+    TxInput,
+    TxOutput,
+    TxOutputDatum as Datum,
+    TxOutputId,
+    ValidatorHash,
+    Value
+} from "@helios-lang/ledger"
+export {
+    Bip32PrivateKey,
+    Emulator as NetworkEmulator,
+    WalletHelper
+} from "@helios-lang/tx-utils"
+export {
+    ByteArrayData,
+    ConstrData,
+    IntData,
+    ListData,
+    MapData,
+    UplcDataValue,
+    UplcProgramV1,
+    UplcProgramV2,
+    UplcProgramV3
+} from "@helios-lang/uplc"
+
+/**
+ * @typedef {import("@helios-lang/crypto").NumberGenerator} NumberGenerator
+ * @typedef {import("@helios-lang/ledger").NetworkParams} NetworkParams
+ * @typedef {import("@helios-lang/tx-utils").Network} Network
+ * @typedef {import("@helios-lang/tx-utils").RegularTx} RegularTx
+ * @typedef {import("@helios-lang/tx-utils").Wallet} Wallet
+ * @typedef {import("@helios-lang/uplc").UplcData} UplcData
+ * @typedef {import("@helios-lang/uplc").UplcProgram} UplcProgram
+ */
+
+/**
+ * @type {NetworkParams}
+ */
+export const rawNetworkEmulatorParams = DEFAULT_NETWORK_PARAMS()
